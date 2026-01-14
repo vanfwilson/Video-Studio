0e9fceadf359681a783be9e5752f7bfa0e8c4315
drvanwilson <157468386+vanfwilson@users.noreply.github.com>
2026-01-14 12:20:05 -0700

    db and video models

---
+++ models/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://username:password@localhost:5432/video_db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
